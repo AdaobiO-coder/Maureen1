1f2990802eafa3e5057397c4e1323023386c5939
Maureen Adaobi <okekeadaobi9@gmail.com>
2022-06-25 08:26:59 -0700

    index.html

---
+++ index.js
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="UTF-8" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <title>Introduction to Js</title>
+  </head>
+  <body>
+    <script language="Javascript" type="text/javascript">
+    var n, h, c
+      n= prompt("OKEKE MAUREEN")
+      h= prompt("1.65")
+      c=prompt("NIGERIA")
+
+      document.write(n + "<br>" + h + "<br>" + c)
+    </script>
+  </body>
+</html>
